397579e5f5d6194708c52a3268f004da5bc023bb
XavierNiu <a@nxw.name>
2021-03-24 23:05:26 +0800

    three sum

--- bytedance/three_sum.go
+++ bytedance/three_sum.go
@@ -1,17 +1,25 @@
 package bytedance
 
-//func threeSum(nums []int) [][]int {
-//	var ret [][]int
-//	numsMap := make(map[int]int, len(nums))
-//	for _, v := range nums {
-//		if _, ok := numsMap[v]; ok {
-//			numsMap[v]++
-//		} else {
-//			numsMap[v] = 1
-//		}
-//	}
-//
-//	for i, v := range nums {
-//
-//	}
-//}
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	var ret [][]int
+	for i, a := range nums {
+		if i > 0 && nums[i-1] == nums[i] { continue }
+		target, j, k := -a, i+1, len(nums)-1
+		for j < k {
+			if nums[j] + nums[k] < target {
+				j++
+			} else if nums[j] + nums[k] > target {
+				k--
+			} else {
+				ret = append(ret, []int{a, nums[j], nums[k]})
+				for ; j < k && nums[j] == nums[j+1]; j++ {}
+				for ; j < k && nums[k] == nums[k-1]; k-- {}
+				j, k = j+1, k-1
+			}
+		}
+	}
+	return ret
+}
