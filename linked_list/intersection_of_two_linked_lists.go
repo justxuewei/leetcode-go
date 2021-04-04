81a37782ca6aa422fe1b1d94d95c2370702f7807
XavierNiu <a@nxw.name>
2021-04-04 15:15:49 +0800

    leetcode 142 && 160

---
+++ linked_list/intersection_of_two_linked_lists.go
@@ -0,0 +1,16 @@
+package linked_list
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	a, b := headA, headB
+
+	for a != b {
+		if a == nil { a = headB } else { a = a.Next }
+		if b == nil { b = headA } else { b = b.Next }
+	}
+
+	return a
+}
--- linked_list/linked_list_circle_II.go
+++ linked_list/linked_list_circle_II.go
@@ -1,5 +1,24 @@
 package linked_list
 
 func detectCycle(head *ListNode) *ListNode {
-	
+	if isCircle, p1 := hasCircle1(head); isCircle {
+		p2 := head
+		for p1 != p2 {
+			p1, p2 = p1.Next, p2.Next
+		}
+		return p1
+	}
+	return nil
+}
+
+func hasCircle1(head *ListNode) (bool, *ListNode) {
+	if head == nil || head.Next == nil { return false, nil }
+	fast, slow := head, head
+	for fast.Next != nil && fast.Next.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
+			return true, slow
+		}
+	}
+	return false, nil
 }
