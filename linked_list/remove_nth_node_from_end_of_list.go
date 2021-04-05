a66f2c82eb956aa0e01e53ae78f9cf77e5e12441
XavierNiu <a@nxw.name>
2021-04-05 14:21:00 +0800

    leetcode 19

---
+++ linked_list/remove_nth_node_from_end_of_list.go
@@ -0,0 +1,28 @@
+package linked_list
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head == nil || n == 0 { return head }
+
+	var p1 *ListNode
+	ret, p2, p3 := head, head, head
+	remaining := n
+
+	for remaining > 0 {
+		p3 = p3.Next
+		remaining--
+	}
+
+	for p3 != nil {
+		p1 = p2
+		p2, p3 = p2.Next, p3.Next
+	}
+
+	//goland:noinspection GoNilness
+	if p1 != nil {
+		p1.Next = p2.Next
+	} else {
+		ret = head.Next
+	}
+
+	return ret
+}
