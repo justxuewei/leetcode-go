94f287368c88169350a9ff6e9ff478bfca0a0ca8
XavierNiu <a@nxw.name>
2021-04-11 10:35:51 +0800

    sync

---
+++ jzoffer/lowest_common_ancestor_of_a_binary_tree.go
@@ -0,0 +1,50 @@
+package jzoffer
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	var pathp, pathq []*TreeNode
+	dfs(root, p, &pathp)
+	dfs(root, q, &pathq)
+	n := min(len(pathp), len(pathq))
+	for i:=1; i<n; i++ {
+		if pathp[i] != pathq[i] {
+			return pathp[i-1]
+		}
+	}
+	return pathp[n-1]
+}
+
+func dfs(root, node *TreeNode, path *[]*TreeNode) bool {
+	if root == nil { return false }
+
+	*path = append(*path, root)
+
+	if node == root { return true }
+
+	if dfs(root.Left, node, path) {
+		return true
+	}
+	if dfs(root.Right, node, path) {
+		return true
+	}
+	// remove history
+	*path = (*path)[:len(*path)-1]
+	return false
+}
+
+func min(lhs, rhs int) int {
+	if lhs < rhs { return lhs }
+	return rhs
+}
+
+func lowestCommonAncestor1(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q { return root }
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	} else if left != nil {
+		return left
+	} else {
+		return right
+	}
+}
---
+++ jzoffer/majority_element.go
@@ -0,0 +1,19 @@
+package jzoffer
+
+// https://leetcode.com/problems/majority-element/
+// https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/
+func majorityElement(nums []int) int {
+	res, count := nums[0], 1
+	for i:=1; i<len(nums); i++ {
+		if count == 0 {
+			res, count = nums[i], 1
+		} else {
+			if res == nums[i] {
+				count++
+			} else {
+				count--
+			}
+		}
+	}
+	return res
+}
