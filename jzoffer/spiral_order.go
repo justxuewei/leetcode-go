64940199a46dddfb7e3c37e6073f9b30b3c4c297
XavierNiu <a@nxw.name>
2021-04-21 16:30:42 +0800

    sync

--- jzoffer/spiral_order.go
+++ jzoffer/spiral_order.go
@@ -42,3 +42,41 @@ func traversal(matrix, visited [][]int, x, y, i int, dir [2]int, ret []int) {
 
 	traversal(matrix, visited, x+dir[0], y+dir[1], i, dir, ret)
 }
+
+func spiralOrder1(matrix [][]int) []int {
+	if len(matrix) == 0 { return nil }
+
+	ret := make([]int, 0, len(matrix)*len(matrix[0]))
+	r, b, l, t := len(matrix[0])-1, len(matrix)-1, 0, 0
+	for {
+		// from left to right
+		for i:=l; i<=r; i++ {
+			ret = append(ret, matrix[t][i])
+		}
+		t++
+		if t > b { break }
+		// from top to bottom
+		for i:=t; i<=b; i++ {
+			ret = append(ret, matrix[i][r])
+		}
+		r--
+		if l > r { break }
+		// from right to left
+		for i:=r; i>=l; i-- {
+			ret = append(ret, matrix[b][i])
+		}
+		b--
+		if t > b { break }
+		// from bottom to top
+		for i:=b; i>=t; i-- {
+			ret = append(ret, matrix[i][l])
+		}
+		l++
+		if l > r {
+			break
+		}
+	}
+
+	return ret
+}
+
