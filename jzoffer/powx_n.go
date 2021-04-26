0e9ed86b747697dc9fd30e3693a5cc53db5c788a
XavierNiu <a@nxw.name>
2021-04-26 13:27:31 +0800

    LeetCode 50

---
+++ jzoffer/powx_n.go
@@ -0,0 +1,44 @@
+package jzoffer
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	} else if n < 0 {
+		n = -n
+		x = 1/x
+	}
+
+	if x == float64(1) {
+		return 1
+	} else if x == float64(-1) {
+		if n % 2 == 0 {
+			return 1
+		}
+		return -1
+	}
+
+	ret := float64(1)
+	for i:=0; i<n; i++ {
+		if ret == 0 { break }
+		ret *= x
+	}
+	return ret
+}
+
+func myPow1(x float64, n int) float64 {
+	if n < 0 {
+		n = -n
+		x = 1/x
+	}
+
+	ret := float64(1)
+	for n > 0 {
+		if n & 1 == 1 {
+			ret *= x
+		}
+		x *= x
+		n >>= 1
+	}
+
+	return ret
+}
