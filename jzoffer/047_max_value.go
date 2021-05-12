f5b3670e56409fc603afb7f02fbbe456f7e0ba16
XavierNiu <a@nxw.name>
2021-05-12 10:39:29 +0800

    剑指Offer47

---
+++ jzoffer/047_max_value.go
@@ -0,0 +1,36 @@
+package jzoffer
+
+import "math"
+
+func maxValue(grid [][]int) int {
+	// init dp array
+	dp := make([][]int, len(grid))
+	for i := range dp {
+		dp[i] = make([]int, len(grid[0]))
+	}
+	dp[0][0] = grid[0][0]
+
+	// dp
+	for i:=0; i<len(grid); i++ {
+		for j:=0; j<len(grid[0]); j++ {
+			if i == 0 && j == 0 { continue }
+			above, left := math.MinInt32, math.MinInt32
+			if i - 1 >= 0 {
+				left = dp[i-1][j]
+			}
+			if j - 1 >= 0 {
+				above = dp[i][j-1]
+			}
+			dp[i][j] = maxInt(left, above) + grid[i][j]
+		}
+	}
+
+	return dp[len(grid)-1][len(grid[0])-1]
+}
+
+func maxInt(lhs, rhs int) int {
+	if lhs > rhs {
+		return lhs
+	}
+	return rhs
+}
