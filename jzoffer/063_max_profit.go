614054dd23b957ddd14d2811b13a47cd03f75721
XavierNiu <a@nxw.name>
2021-05-19 11:01:49 +0800

    剑指Offer63 dp

---
+++ jzoffer/063_max_profit.go
@@ -0,0 +1,15 @@
+package jzoffer
+
+func maxProfit(prices []int) int {
+	if len(prices) < 2 { return 0 }
+	profit, lowestPrice := 0, prices[0]
+	for i:=1; i<len(prices); i++ {
+		if profit <= prices[i] - lowestPrice {
+			profit = prices[i] - lowestPrice
+		}
+		if lowestPrice >= prices[i] {
+			lowestPrice = prices[i]
+		}
+	}
+	return profit
+}
