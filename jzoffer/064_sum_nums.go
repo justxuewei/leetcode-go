6495814396d31fdf808a5c83934390cbc106f135
XavierNiu <a@nxw.name>
2021-05-21 11:09:56 +0800

    剑指Offer64

---
+++ jzoffer/064_sum_nums.go
@@ -0,0 +1,13 @@
+package jzoffer
+
+func sumNums(n int) int {
+	var f func(ans *int, n int) bool
+	f = func(ans *int, n int) bool {
+		*ans += n
+		return n>0 && f(ans, n-1)
+	}
+
+	var ret int
+	f(&ret, n)
+	return ret
+}
