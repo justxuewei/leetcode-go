e104810f4c6e19e7926a679adce01c232769aa6b
XavierNiu <a@nxw.name>
2021-05-28 13:00:04 +0800

    sync

---
+++ jzoffer/041_median_finder.go
@@ -0,0 +1,19 @@
+package jzoffer
+
+// TODO: Learn how go implements a heap container?(ref:https://www.cnblogs.com/huxianglin/p/6925119.html)
+
+type MedianFinder struct {
+
+}
+
+func NewMedianFinder() MedianFinder {
+	return MedianFinder{}
+}
+
+func (f *MedianFinder) AddNum(num int) {
+
+}
+
+func (f *MedianFinder) FindMedian() float64 {
+	return 0
+}
