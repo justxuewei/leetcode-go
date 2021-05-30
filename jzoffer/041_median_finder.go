010770f7bd1857d184456755b9a6abc61609d3ea
XavierNiu <a@nxw.name>
2021-05-30 13:10:36 +0800

    剑指Offer41

--- jzoffer/041_median_finder.go
+++ jzoffer/041_median_finder.go
@@ -1,6 +1,39 @@
 package jzoffer
 
+import "container/heap"
+
+type MedianFinder struct {
+	maxh *MaxHeap
+	minh *MinHeap
+}
+
+func NewMedianFinder() MedianFinder {
+	// heap.Init() is not required here.
+	return MedianFinder{
+		maxh: &MaxHeap{make(Heap, 0)},
+		minh: &MinHeap{make(Heap, 0)},
+	}
+}
+
+func (f *MedianFinder) AddNum(num int) {
+	if f.minh.Len() == f.maxh.Len() {
+		heap.Push(f.maxh, num)
+		heap.Push(f.minh, heap.Pop(f.maxh))
+	} else {
+		heap.Push(f.minh, num)
+		heap.Push(f.maxh, heap.Pop(f.minh))
+	}
+}
+
+func (f *MedianFinder) FindMedian() float64 {
+	if f.minh.Len() == f.maxh.Len() {
+		return (float64(f.minh.Heap[0]) + float64(f.maxh.Heap[0])) / 2.0
+	}
+	return float64(f.minh.Heap[0])
+}
+
 // TODO: Learn how go implements a heap container?(ref:https://www.cnblogs.com/huxianglin/p/6925119.html)
+// ----- Heap ------
 
 type Heap []int
 
@@ -22,6 +55,8 @@ func (h *Heap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
 
+// ----- MinHeap -----
+
 type MinHeap struct {
 	Heap
 }
@@ -30,39 +65,12 @@ func (h *MinHeap) Less(i, j int) bool {
 	return h.Heap[i] < h.Heap[j]
 }
 
+// ----- MaxHeap -----
+
 type MaxHeap struct {
 	Heap
 }
 
 func (h *MaxHeap) Less(i, j int) bool {
 	return h.Heap[i] > h.Heap[j]
-}
-
-type MedianFinder struct {
-	maxh *MaxHeap
-	minh *MinHeap
-}
-
-func NewMedianFinder() MedianFinder {
-	return MedianFinder{
-		maxh: &MaxHeap{make(Heap, 0)},
-		minh: &MinHeap{make(Heap, 0)},
-	}
-}
-
-func (f *MedianFinder) AddNum(num int) {
-	if f.minh.Len() == f.maxh.Len() {
-		f.maxh.Push(num)
-		f.minh.Push(f.maxh.Pop())
-	} else {
-		f.minh.Push(num)
-		f.maxh.Push(f.minh.Pop())
-	}
-}
-
-func (f *MedianFinder) FindMedian() float64 {
-	if f.minh.Len() == f.maxh.Len() {
-		return (float64(f.minh.Heap[0]) + float64(f.maxh.Heap[0])) / 2.0
-	}
-	return float64(f.minh.Heap[0])
 }
