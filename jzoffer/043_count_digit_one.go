920a686d559538fe4075e6fc587f1ce9409342c6
XavierNiu <a@nxw.name>
2021-06-01 12:14:10 +0800

    剑指Offer43

--- jzoffer/043_count_digit_one.go
+++ jzoffer/043_count_digit_one.go
@@ -1,6 +1,20 @@
 package jzoffer
 
 func countDigitOne(n int) int {
-	
-	return 0
+	var ret int
+	digit := 1
+	tmp := n
+	for tmp > 0 {
+		switch tmp % 10 {
+		case 0:
+			ret += tmp / 10 * digit
+		case 1:
+			ret += tmp / 10 * digit + n % digit + 1
+		default:
+			ret += (tmp / 10 + 1) * digit
+		}
+		digit *= 10
+		tmp /= 10
+	}
+	return ret
 }
