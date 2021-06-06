9f1fbeb7486d6568620eeb544742e076a411a57a
XavierNiu <a@nxw.name>
2021-06-06 13:15:16 +0800

    剑指Offer59I 双端队列

---
+++ jzoffer/059_I_max_sliding_window.go
@@ -0,0 +1,26 @@
+package jzoffer
+
+func maxSlidingWindow(nums []int, k int) []int {
+	if len(nums) < 2 {
+		return nums
+	}
+	monotonicQueue := make([]int, 1)
+	ret := make([]int, 0, len(nums))
+	for i:=0; i<len(nums); i++ {
+		// remove head element if it is not in the sliding window
+		if i - k >= 0 && nums[i - k] == monotonicQueue[0] {
+			monotonicQueue = monotonicQueue[1:]
+		}
+		for j:=0; j<len(monotonicQueue); j++ {
+			if nums[i] > monotonicQueue[j] {
+				monotonicQueue = monotonicQueue[:j]
+				break
+			}
+		}
+		monotonicQueue = append(monotonicQueue, nums[i])
+		if i >= k - 1 {
+			ret = append(ret, monotonicQueue[0])
+		}
+	}
+	return ret
+}
