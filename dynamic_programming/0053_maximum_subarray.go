ade1e84c7e77daab9d7a7533b95bfb377bcaccd8
XavierNiu <a@nxw.name>
2021-06-07 13:22:44 +0800

    0053 dp

---
+++ dynamic_programming/0053_maximum_subarray.go
@@ -0,0 +1,19 @@
+package dynamic_programming
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	ret := nums[0]
+	for i:=1; i<len(nums); i++ {
+		if nums[i-1] + nums[i] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if ret < nums[i] {
+			ret = nums[i]
+		}
+	}
+
+	return ret
+}
