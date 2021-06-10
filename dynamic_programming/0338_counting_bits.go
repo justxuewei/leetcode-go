4ae873b2aaca02d927814c886d6338537c829d0c
XavierNiu <a@nxw.name>
2021-06-10 11:36:29 +0800

    0338 done

--- dynamic_programming/0338_counting_bits.go
+++ dynamic_programming/0338_counting_bits.go
@@ -2,10 +2,9 @@ package dynamic_programming
 
 func countBits(n int) []int {
 	ret := make([]int, n+1)
-	ret[0], ret[1] = 0, 1
-	max, cntmax := 2, 2
-	for i:=2; i<=n; i++ {
-		if 
+	ret[0] = 0
+	for i:=1; i<=n; i++ {
+		ret[i] = i & 1 + ret[i >> 1]
 	}
 	return ret
 }
