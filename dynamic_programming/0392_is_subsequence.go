0cbd4deb40e71a0b5c879a346744556a6ed66a76
XavierNiu <a@nxw.name>
2021-06-11 11:29:00 +0800

    0392 done

---
+++ dynamic_programming/0392_is_subsequence.go
@@ -0,0 +1,16 @@
+package dynamic_programming
+
+func isSubsequence(s string, t string) bool {
+	if len(s) == 0 { return true }
+	if len(t) == 0 { return false }
+	matchidx := 0
+	for i := range t {
+		if t[i] == s[matchidx] {
+			matchidx++
+		}
+		if matchidx == len(s) {
+			return true
+		}
+	}
+	return false
+}
