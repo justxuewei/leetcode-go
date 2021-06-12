f492f16977da5cf40ae6cb04122bde2211b2fb29
XavierNiu <a@nxw.name>
2021-06-12 15:46:07 +0800

    0746 done

---
+++ dynamic_programming/0746_min_cost_climbing_stairs.go
@@ -0,0 +1,18 @@
+package dynamic_programming
+
+func minCostClimbingStairs(cost []int) int {
+	if len(cost) > 2 {
+		for i:=2; i<len(cost); i++ {
+			cost[i] = cost[i] + min(cost[i-2], cost[i-1])
+		}
+	}
+
+	return min(cost[len(cost)-2], cost[len(cost)-1])
+}
+
+func min(left, right int) int {
+	if left < right {
+		return left
+	}
+	return right
+}
