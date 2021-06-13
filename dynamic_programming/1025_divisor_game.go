bc5abf5889b8d6b28a175b9d5fd2fe4d4bf488e6
XavierNiu <a@nxw.name>
2021-06-13 12:45:04 +0800

    1025 done

---
+++ dynamic_programming/1025_divisor_game.go
@@ -0,0 +1,15 @@
+package dynamic_programming
+
+func divisorGame(n int) bool {
+	dp := make([]bool, n+1)
+	dp[1] = false
+	for i:=2; i<=n; i++ {
+		for j:=1; j<i; j++ {
+			if i % j == 0 && !dp[i-j] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}
