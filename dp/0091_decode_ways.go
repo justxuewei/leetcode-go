0de03e825e79c3e7b767f275992daa0be2545048
XavierNiu <a@nxw.name>
2021-07-09 13:01:56 +0800

    0091 init

---
+++ dp/0091_decode_ways.go
@@ -0,0 +1,40 @@
+package dp
+
+import "strconv"
+
+func numDecodings(s string) int {
+	if s[0] == '0' {
+		return 0
+	}
+
+	if len(s) == 1 {
+		return 1
+	}
+
+	dp0, dp1 := 1, 1
+	for i:=1; i<len(s); i++ {
+		cur, _ := strconv.Atoi(s[i:i+1])
+		prev, _ := strconv.Atoi(s[i-1:i+1])
+
+		if cur == 0 {
+			if prev > 0 && prev <= 26 {
+				// reset
+				dp0, dp1 = 0, 1
+				continue
+			} else {
+				return 0
+			}
+		}
+
+		var dpnow int
+		if cur > 0 {
+			dpnow += dp1
+		}
+		if prev > 0 && prev < 27 {
+			dpnow += dp0
+		}
+		dp0, dp1 = dp1, dpnow
+	}
+
+	return dp1
+}
