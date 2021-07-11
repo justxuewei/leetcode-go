89a70e2bbdd3dd33b1cc5ff4f1c8ba2c22868fd7
XavierNiu <a@nxw.name>
2021-07-11 12:18:57 +0800

    0096 init

---
+++ dp/0096_unique_binary_search_trees.go
@@ -0,0 +1,18 @@
+package dp
+
+func numTrees(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	dp[1] = 1
+
+	for i:=2; i<=n; i++ {
+		for j:=1; j<=i; j++ {
+			// TODO
+			dp[i] += dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
