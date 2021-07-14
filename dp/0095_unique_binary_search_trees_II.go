3ebff17b9fbf09e4cfe34ac3f3595ec4c650d4d4
XavierNiu <a@nxw.name>
2021-07-14 12:35:10 +0800

    0095 sync

--- dp/0095_unique_binary_search_trees_II.go
+++ dp/0095_unique_binary_search_trees_II.go
@@ -3,7 +3,26 @@ package dp
 import "github.com/xavier-niu/leetcode/ds"
 
 func generateTrees(n int) []*ds.TreeNode {
-	var trees []*ds.TreeNode
+	if n == 1 {
+		return []*ds.TreeNode{{Val: 1}}
+	}
 
-	return trees
+	var dp [][]*ds.TreeNode
+	// dp[0]
+	dp = append(dp, []*ds.TreeNode{})
+	// dp[1]
+	dp = append(dp, []*ds.TreeNode{{Val: 1}})
+
+	for i:=2; i<=n; i++ {
+		for j:=1; j<=i; j++ {
+			// root == j
+			// ?
+			//nleft, nright := j-1, i-j
+			//for l:=0; l<nleft; l++ {
+			//	for r:=nright;
+			//}
+		}
+	}
+
+	return dp[n]
 }
