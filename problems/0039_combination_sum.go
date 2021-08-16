73241eb26248603a328a98e97d7b4f323abe0e09
XavierNiu <a@nxw.name>
2021-08-16 19:45:19 +0800

    0039 done

---
+++ problems/0039_combination_sum.go
@@ -0,0 +1,36 @@
+package problems
+
+func combinationSum(candidates []int, target int) [][]int {
+	var (
+		ret [][]int
+		path []int
+	)
+	combinationSumDfs(candidates, 0, target, path, &ret)
+
+	return ret
+}
+
+func combinationSumDfs(candidates []int, begin, remaining int, path []int, ret *[][]int) {
+	if remaining < 0 {
+		return
+	}
+
+	if remaining == 0 {
+		*ret = append(*ret, deepCopy(path))
+	}
+
+	path = append(path, 0)
+
+	for i:=begin; i<len(candidates); i++ {
+		path[len(path)-1] = candidates[i]
+		combinationSumDfs(candidates, i, remaining-candidates[i], path, ret)
+	}
+}
+
+func deepCopy(arr []int) []int {
+	ret := make([]int, len(arr))
+	for i := range arr {
+		ret[i] = arr[i]
+	}
+	return ret
+}
