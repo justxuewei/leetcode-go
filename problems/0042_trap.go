84fb03a5292f77cb3250784c0c73587ce90721be
XavierNiu <a@nxw.name>
2021-08-21 12:09:52 +0800

    0042 done

--- problems/0042_trap.go
+++ problems/0042_trap.go
@@ -1,7 +1,8 @@
 package problems
 
 func trap(height []int) int {
-	var idx, ret int
+	var ret int
+	idx := -1
 	var monostack []int
 
 	for i, v := range height {
@@ -11,12 +12,29 @@ func trap(height []int) int {
 		}
 	}
 
-	monostack = append(monostack, idx)
-	for i:=idx; i<len(height); i++ {
-		if height[i] >= height[monostack[len(monostack)-1]] {
+	if idx == -1 || idx == len(height)-1 {
+		return ret
+	}
 
+	monostack = append(monostack, idx)
+	for i := idx + 1; i < len(height); i++ {
+		for len(monostack) > 0 && height[i] >= height[monostack[len(monostack)-1]] {
+			// the front is existed
+			if len(monostack) != 1 {
+				ret += (trapMin(height[monostack[len(monostack)-2]], height[i]) - height[monostack[len(monostack)-1]]) *
+					(i - monostack[len(monostack)-2] - 1)
+			}
+			monostack = monostack[:len(monostack)-1]
 		}
+		monostack = append(monostack, i)
 	}
 
 	return ret
+}
+
+func trapMin(lhs, rhs int) int {
+	if lhs < rhs {
+		return lhs
+	}
+	return rhs
 }
