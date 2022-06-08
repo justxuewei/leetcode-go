d61fe69645f3a357342f5c2cfc816f089d61e154
Xuewei Niu <justxuewei@apache.org>
2022-06-08 14:42:36 +0800

    feat(problems): finish 22
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- problems/0022_generate_parenthesis.go
+++ problems/0022_generate_parenthesis.go
@@ -1,11 +1,22 @@
 package problems
 
+import "fmt"
+
 func generateParenthesis(n int) []string {
-	parenthesis := []string{"()"}
+	dp := make([][]string, 9)
+	dp[0] = []string{""}
+	dp[1] = []string{"()"}
 
 	for i := 2; i <= n; i++ {
-		
+		dp[i] = make([]string, 0, 8)
+		for j := 0; j < i; j++ {
+			for _, p := range dp[j] {
+				for _, q := range dp[i-1-j] {
+					dp[i] = append(dp[i], fmt.Sprintf("(%s)%s", p, q))
+				}
+			}
+		}
 	}
 
-	return []string{}
+	return dp[n]
 }
