beaad4cc7a47ca84d4105359e3c7a7a26147ffc8
Xuewei Niu <justxuewei@apache.org>
2022-06-14 21:03:24 +0800

    feat: problems 179
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- problems/0179_largest_number.go
+++ problems/0179_largest_number.go
@@ -1,6 +1,7 @@
 package problems
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -14,26 +15,25 @@ func largestNumber(nums []int) string {
 	}
 
 	quickSort(numStrings)
-	return strings.Join(numStrings, "")
-}
-
-func less(lhs, rhs string) bool {
-	var (
-		lhsIdx = len(lhs)
-		rhsIdx = len(rhs)
-	)
-
-	for lhsIdx >= 0 && rhsIdx >= 0 {
-		lhsIdx--
-		rhsIdx--
-
-		if lhs[lhsIdx] != rhs[rhsIdx] {
-			return lhs[lhsIdx] < rhs[rhsIdx]
+	str := strings.Join(numStrings, "")
+	var zero int
+	for ; zero < len(str)-1; zero++ {
+		if str[zero] != '0' {
+			break
 		}
 	}
 
-	if lhsIdx < 0 &&  {
+	return str[zero:]
+}
 
+func less(lhs, rhs string) bool {
+	lr := fmt.Sprintf("%s%s", lhs, rhs)
+	rl := fmt.Sprintf("%s%s", rhs, lhs)
+
+	for i := 0; i < len(lr); i++ {
+		if lr[i] != rl[i] {
+			return lr[i] < rl[i]
+		}
 	}
 
 	return false
@@ -58,4 +58,50 @@ func quickSort(arr []string) {
 	arr[0], arr[r] = arr[r], arr[0]
 	quickSort(arr[:r])
 	quickSort(arr[r+1:])
+}
+
+func largestNumber1(num []int) string {
+	quickSort1(num)
+
+	if num[0] == 0 {
+		return "0"
+	}
+	
+	sb := new(strings.Builder)
+	for _, n := range num {
+		sb.WriteString(strconv.Itoa(n))
+	}
+	return sb.String()
+}
+
+func less1(lhs, rhs int) bool {
+	sx, sy := 10, 10
+	for sx <= lhs {
+		sx *= 10
+	}
+	for sy <= rhs {
+		sy *= 10
+	}
+	return rhs+sy*lhs <= lhs+sx*rhs
+}
+
+func quickSort1(arr []int) {
+	if len(arr) <= 1 {
+		return
+	}
+
+	l, r := 1, len(arr)-1
+
+	for l <= r {
+		if less1(arr[r], arr[0]) {
+			r--
+			continue
+		}
+		arr[l], arr[r] = arr[r], arr[l]
+		l++
+	}
+
+	arr[0], arr[r] = arr[r], arr[0]
+	quickSort1(arr[:r])
+	quickSort1(arr[r+1:])
 }
--- problems/0179_largest_number_test.go
+++ problems/0179_largest_number_test.go
@@ -10,8 +10,8 @@ func TestLess(t *testing.T) {
 	assert.True(t, less("10", "2"))
 	assert.True(t, less("98", "9"))
 	assert.True(t, !less("9", "9"))
-	assert.True(t, less("3432", "34323"))
-	assert.True(t, less("34329", "3432"))
+	assert.True(t, less("34323", "3432"))
+	assert.True(t, less("3432", "34329"))
 }
 
 func TestQuickSort(t *testing.T) {
@@ -24,4 +24,10 @@ func TestQuickSort(t *testing.T) {
 	case2Expect := []string{"3432", "34323"}
 	quickSort(case2)
 	assert.Equal(t, case2Expect, case2)
+}
+
+func TestLargestNumber1(t *testing.T) {
+	case1 := []int{0,9,8,7,6,5,4,3,2,1}
+	case1Expect := "9876543210"
+	assert.Equal(t, case1Expect, largestNumber1(case1))
 }
