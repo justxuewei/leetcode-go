6b3298b8c633631f4fe5e8de5084fba7a2bc9006
Xuewei Niu <justxuewei@apache.org>
2022-06-16 21:27:09 +0800

    feat: problems 909
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

--- problems/0909_snakes_and_ladders.go
+++ problems/0909_snakes_and_ladders.go
@@ -1,35 +1,74 @@
 package problems
 
-// func snakesAndLadders(board [][]int) int {
-// 	n := len(board)
-// }
-
-func getCoordination(n, num int) (row int, col int) {
-	row = n - num/n - 1
-	if row%2 != n%2 {
-		if (num / n) * n == 0 {
-			col = num - 1
-		} else {
-			col = (num / n) * n - 1
+func snakesAndLadders(board [][]int) int {
+	var (
+		stepZero = &ListNode{
+			Val:  1,
+			Next: nil,
 		}
-	} else {
-		col = n - (num / n) * n
+		head = &ListNode{
+			Val:  0,
+			Next: stepZero,
+		}
+		stepLast = stepZero
+		current  *ListNode
+		next     *ListNode
+		nextVal  int
+		tail     = stepZero
+		step     = 1
+		i        = 0
+		n2       = len(board) * len(board)
+		row, col int
+		numSet   = make(map[int]int8)
+		ok       bool
+	)
+
+	for head.Next != nil {
+		current = head.Next
+
+		for i = 1; i <= 6; i++ {
+			nextVal = current.Val + i
+			
+			row, col = getCoordination(len(board), nextVal)
+			if board[row][col] != -1 {
+				nextVal = board[row][col]
+			}
+
+			if _, ok = numSet[nextVal]; ok {
+				continue
+			} else {
+				numSet[nextVal] = 0
+			}
+			if nextVal >= n2 {
+				return step
+			}
+
+			next = &ListNode{Val: nextVal}
+
+			tail.Next = next
+			tail = next
+		}
+
+		if current.Val == stepLast.Val {
+			stepLast = tail
+			step++
+		}
+
+		head.Next = current.Next
 	}
-	return
+
+	return -1
+
 }
 
-func getNum(n, row, col int) int {
-	var (
-		revertRow    = n - (row + 1)
-		multipler    = 1
-		startedValue int
-	)
-	if revertRow%2 != 0 {
-		multipler = -1
-		startedValue = (revertRow + 1) * n
-	} else {
-		startedValue = revertRow*n + 1
+func getCoordination(n, num int) (row int, col int) {
+	row = n - (num-1)/n - 1
+	col = num%n - 1
+	if col < 0 {
+		col = n - 1
 	}
-
-	return startedValue + multipler*col
+	if row%2 == n%2 {
+		col = (n - 1) - col
+	}
+	return
 }
--- problems/0909_snakes_and_ladders_test.go
+++ problems/0909_snakes_and_ladders_test.go
@@ -6,16 +6,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetNum(t *testing.T) {
-	assert.Equal(t, 1, getNum(4, 3, 0))
-	assert.Equal(t, 4, getNum(4, 3, 3))
-	assert.Equal(t, 16, getNum(4, 0, 0))
-	assert.Equal(t, 10, getNum(4, 1, 1))
-}
-
 func TestGetCoordination(t *testing.T) {
 	case1Row, case1Col := getCoordination(4, 1)
 	case1ExpectRow, case1ExpectCol := 3, 0
 	assert.Equal(t, case1ExpectRow, case1Row)
 	assert.Equal(t, case1ExpectCol, case1Col)
+
+	case2Row, case2Col := getCoordination(4, 16)
+	case2ExpectRow, case2ExpectCol := 0, 0
+	assert.Equal(t, case2ExpectRow, case2Row)
+	assert.Equal(t, case2ExpectCol, case2Col)
+
+	case3Row, case3Col := getCoordination(4, 12)
+	case3ExpectRow, case3ExpectCol := 1, 3
+	assert.Equal(t, case3ExpectRow, case3Row)
+	assert.Equal(t, case3ExpectCol, case3Col)
+
+	case4Row, case4Col := getCoordination(4, 6)
+	case4ExpectRow, case4ExpectCol := 2, 2
+	assert.Equal(t, case4ExpectRow, case4Row)
+	assert.Equal(t, case4ExpectCol, case4Col)
+}
+
+func TestSnakesAndLadders(t *testing.T) {
+	case1 := [][]int{
+		{-1, -1, -1, -1, -1, -1},
+		{-1, -1, -1, -1, -1, -1},
+		{-1, -1, -1, -1, -1, -1},
+		{-1, 35, -1, -1, 13, -1},
+		{-1, -1, -1, -1, -1, -1},
+		{-1, 15, -1, -1, -1, -1}}
+	assert.Equal(t, 4, snakesAndLadders(case1))
+
+	case2 := [][]int{
+		{-1, 4, -1},
+		{6, 2, 6},
+		{-1, 3, -1},
+	}
+	assert.Equal(t, 2, snakesAndLadders(case2))
 }
