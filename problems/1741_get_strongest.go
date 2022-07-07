a2e75d91eea0b30d9bb3ca07eee81782c197e5c9
Xuewei Niu <justxuewei@apache.org>
2022-07-07 14:17:50 +0800

    feat: problems 1741
    Signed-off-by: Xuewei Niu <justxuewei@apache.org>

---
+++ problems/1741_get_strongest.go
@@ -0,0 +1,34 @@
+package problems
+
+import (
+	"math"
+	"sort"
+)
+
+func getStrongest(arr []int, k int) []int {
+	if len(arr) <= k {
+		return arr
+	}
+	sort.Ints(arr)
+	m := arr[(len(arr)-1)/2]
+
+	var (
+		l   = 0
+		r   = len(arr) - 1
+		ret = make([]int, 0, k)
+	)
+
+	for len(ret) < k && l < r {
+		if math.Abs(float64(arr[l]-m)) <= math.Abs(float64(arr[r]-m)) {
+			ret = append(ret, arr[r])
+			r--
+		} else {
+			ret = append(ret, arr[l])
+			l++
+		}
+	}
+
+	if 
+
+	return ret
+}
